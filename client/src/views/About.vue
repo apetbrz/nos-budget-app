d16252c5296b483c107164cd364de0ce6f0ed0d1
arthurassists <apetbrz@gmail.com>
2024-06-06 15:30:31 -0400

    better placeholder about page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
+  <div class="about main-element">
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<style scoped>
+.about {
+  padding: 2rem 2rem 16rem 2rem;
+  text-align: center;
+}
+h1 {
+  margin: 2rem;
+}
+</style>
